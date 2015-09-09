f783cc1a381bad60a59b86fa5d6119b1ce3d6ff7
Tom Tang <tly1980@gmail.com>
2015-09-09 11:12:33 +1000

    adding delimeter and tsv support

--- csv2mdtable.py
+++ csv2mdtable.py
@@ -2,19 +2,44 @@
 import argparse
 import sys
 import csv
+import re
 
 AP = argparse.ArgumentParser("csv2mdtable")
 AP.add_argument("--src", default=None)
+AP.add_argument("--replace", default=None, type=str)
+AP.add_argument("--delimiter", "-d", default=",", type=str)
+AP.add_argument("--tsv", action="store_const", const='\t', dest="delimiter")
 
-def process(csvf, out=sys.stdout):
-    csvout = csv.writer(out, delimiter='|')
-    for i in csvf:
-        csvout.write(i)
+def process_doc(fin, fout=sys.stdout):
+    pat = re.compile('```csv(.*)```')
+    cnt = fin.read()
+    pattern = re.compile()
+    def search(content, start=0):
+        content.find()
+        pass
+    pass
+
+def line_aft_header(headers):
+    a = ':--'
+    lst =[ a + (len(h) - 2 ) * '-' if len(h) > 2 else a for h in headers]
+    return '|%s|\n' % '|'.join(lst)
+
+def process(fin, fout=sys.stdout, has_header=True, delimiter=","):
+    csvf = csv.reader(fin, delimiter=delimiter)
+
+    for i, r in enumerate(csvf):
+        fout.write('|%s|\n' % '|'.join(r) )
+        if i == 0:
+            if has_header:
+                fout.write(line_aft_header(r))
+        fout.flush()
 
 def main(args):
-    f = sys.stdin if not args.src:
-    process(f)
+    f = sys.stdin if not args.src else open(args.src, "rb")
+    process(f, delimiter=args.delimiter)
+    f.close()
 
 
-if name == '__main__':
+if __name__ == '__main__':
     main(AP.parse_args())
+
